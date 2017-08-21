8054e671b33cddf2c3a7ca094ffef6b59a193373
Chanon Jenakom <chanonjenakom@gmail.com>
2017-08-21 15:33:49 +0700

    Finish 'Register Buyer' API

--- buyers/views.py
+++ buyers/views.py
@@ -1,5 +1,25 @@
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import Buyer
+
+@csrf_exempt
+def register(request):
+    if(request.method != 'POST'):
+        return
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    first_name = request.POST.get('first_name')
+    last_name = request.POST.get('last_name')
+    address = request.POST.get('address')
+    buyer = Buyer(
+        username=username,
+        password=password,
+        first_name=first_name,
+        last_name=last_name,
+        address=address
+    )
+    buyer.save()
+    return HttpResponse(status=204)
 
 def current_buyer(request):
     if 'id' in request.COOKIES:
