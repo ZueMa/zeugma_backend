495c109c5586cb8196b6155bc41d922c2d684415
Chanon Jenakom <chanonjenakom@gmail.com>
2017-08-21 13:37:20 +0700

    Finish 'Retrieve Current Buyer' API

--- buyers/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Buyer
-
-class BuyerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Buyer
-        fields = ('username', 'first_name', 'last_name', 'address')
--- buyers/views.py
+++ buyers/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import Buyer
+
+def current_buyer(request):
+    buyer = Buyer.objects.get(id=1)
+    return JsonResponse({
+        'id': buyer.id,
+        'username': buyer.username,
+        'first_name': buyer.first_name,
+        'last_name': buyer.last_name,
+        'address': buyer.address
+    })
