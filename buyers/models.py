3abb5129950047b7c22a5e35ed294bbe119bbf53
Chanon Jenakom <chanonjenakom@gmail.com>
2017-08-30 17:42:40 +0700

    Create Cart model and relations

--- buyers/models.py
+++ buyers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from products.models import Product
 
 class Buyer(models.Model):
     username = models.CharField(max_length=32)
@@ -9,3 +10,23 @@ class Buyer(models.Model):
 
     def __str__(self):
         return self.username
+
+class Cart(models.Model):
+    is_purchased = models.BooleanField(default=False)
+    buyer = models.ForeignKey(Buyer)
+    items = models.ManyToManyField(
+        Product,
+        through='ProductCart',
+        through_fields=('cart', 'product')
+    )
+
+    def __str__(self):
+        return 'Cart #{}'.format(self.id)
+
+class ProductCart(models.Model):
+    num_items = models.IntegerField()
+    cart = models.ForeignKey(Cart)
+    product = models.ForeignKey(Product)
+
+    def __str__(self):
+        return 'Product #{} in Cart #{}'.format(self.product.id, self.cart.id)
