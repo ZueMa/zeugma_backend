a5bff493a5e2e3b25eb13bfb084533b3a417d151
Chanon Jenakom <chanonjenakom@gmail.com>
2017-09-05 00:57:17 +0700

    Remove unnecessary empty lines and commas - buyers

--- src/buyers/migrations/0002_auto_20170901_1513.py
+++ src/buyers/migrations/0002_auto_20170901_1513.py
@@ -5,30 +5,29 @@ from __future__ import unicode_literals
 from django.db import migrations, models
 import django.db.models.deletion
 
-
 class Migration(migrations.Migration):
 
     initial = True
 
     dependencies = [
         ('products', '0001_initial'),
-        ('buyers', '0001_initial'),
+        ('buyers', '0001_initial')
     ]
 
     operations = [
         migrations.AddField(
             model_name='productcart',
             name='product',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='products.Product'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='products.Product')
         ),
         migrations.AddField(
             model_name='cart',
             name='buyer',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='buyers.Buyer'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='buyers.Buyer')
         ),
         migrations.AddField(
             model_name='cart',
             name='items',
-            field=models.ManyToManyField(through='buyers.ProductCart', to='products.Product'),
-        ),
+            field=models.ManyToManyField(through='buyers.ProductCart', to='products.Product')
+        )
     ]
