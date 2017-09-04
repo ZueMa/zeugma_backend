15ab1a93559623dc9f4607c0d13271d9cc0cdf73
oatThanut <oat_thanut@hotmail.com>
2017-09-04 19:17:50 +0700

    Finish 'Retrieve Purchase History' API

--- buyers/urls.py
+++ buyers/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^(?P<buyer_id>\w{1,100})/cart/$', views.retrieve_cart),
     url(r'^(?P<buyer_id>\w{1,100})/cart/items/$', views.add_item_to_cart),
     url(r'^(?P<buyer_id>\w{1,100})/cart/items/(?P<item_id>\w{1,100})/$', views.update_and_delete_item),
-    url(r'^(?P<buyer_id>\w{1,100})/cart/purchase/$', views.purchase_cart)
+    url(r'^(?P<buyer_id>\w{1,100})/cart/purchase/$', views.purchase_cart),
+    url(r'^(?P<buyer_id>\w{1,100})/purchases/$', views.purchase_history)
 ]
