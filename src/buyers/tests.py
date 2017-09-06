3c493e2adcedf9ed12c469bee9a5322b89a9b1a7
Chanon Jenakom <chanonjenakom@gmail.com>
2017-09-06 13:11:21 +0700

    Create test suites - 'Retreive Buyer's Information' API

--- src/buyers/tests.py
+++ src/buyers/tests.py
@@ -11,7 +11,14 @@ class BuyersTestCase(TestCase):
 
     @classmethod
     def setUpTestData(self):
-        pass
+        self.buyer = Buyer(
+            username='jimmyXavier',
+            password='12345678',
+            first_name='James',
+            last_name='McAvoy',
+            address='100 Universal City Plaza, Universal City, Los Angeles'
+        )
+        self.buyer.save()
 
     def test_buyer_should_register_with_the_right_credentials(self):
         response = self.client.post(
@@ -28,11 +35,32 @@ class BuyersTestCase(TestCase):
 
         self.assertEqual(response.status_code, 204)
 
-    def test_server_should_return_405_with_wrong_HTTP_methods(self):
+    def test_server_should_return_405_with_wrong_HTTP_methods_for_buyer_registration(self):
         first_response = self.client.get('/buyers/')
         second_response = self.client.put('/buyers/')
         third_response = self.client.patch('/buyers/')
         fourth_response = self.client.delete('/buyers/')
+
+        self.assertEqual(first_response.status_code, 405)
+        self.assertEqual(second_response.status_code, 405)
+        self.assertEqual(third_response.status_code, 405)
+        self.assertEqual(fourth_response.status_code, 405)
+
+    def test_buyer_should_retrieve_their_information(self):
+        response = self.client.get('/buyers/1/')
+
+        self.assertEqual(response.json()['buyer_id'], 1)
+        self.assertEqual(response.json()['username'], 'jimmyXavier')
+        self.assertEqual(response.json()['first_name'], 'James')
+        self.assertEqual(response.json()['last_name'], 'McAvoy')
+        self.assertEqual(response.json()['address'], '100 Universal City Plaza, Universal City, Los Angeles')
+        self.assertEqual(response.status_code, 200)
+
+    def test_server_should_return_405_with_wrong_HTTP_methods_for_buyer_information(self):
+        first_response = self.client.post('/buyers/1/')
+        second_response = self.client.put('/buyers/1/')
+        third_response = self.client.patch('/buyers/1/')
+        fourth_response = self.client.delete('/buyers/1/')
 
         self.assertEqual(first_response.status_code, 405)
         self.assertEqual(second_response.status_code, 405)
