2a30b21e6a9ffae118d5cc9b29838261c0c53555
Chanon Jenakom <chanonjenakom@gmail.com>
2017-09-06 12:55:27 +0700

    Create test suites - 'Register Buyer' API

--- src/buyers/tests.py
+++ src/buyers/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Buyer
 
-# Create your tests here.
+import json
+
+class BuyersTestCase(TestCase):
+
+    @classmethod
+    def setUp(self):
+        self.client = Client()
+
+    @classmethod
+    def setUpTestData(self):
+        pass
+
+    def test_buyer_should_register_with_the_right_credentials(self):
+        response = self.client.post(
+            '/buyers/',
+            json.dumps({
+                'username': 'jimmyXavier',
+                'password': '12345678',
+                'first_name': 'James',
+                'last_name': 'McAvoy',
+                'address': '100 Universal City Plaza, Universal City, Los Angeles'
+            }),
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, 204)
+
+    def test_server_should_return_405_with_wrong_HTTP_methods(self):
+        first_response = self.client.get('/buyers/')
+        second_response = self.client.put('/buyers/')
+        third_response = self.client.patch('/buyers/')
+        fourth_response = self.client.delete('/buyers/')
+
+        self.assertEqual(first_response.status_code, 405)
+        self.assertEqual(second_response.status_code, 405)
+        self.assertEqual(third_response.status_code, 405)
+        self.assertEqual(fourth_response.status_code, 405)
