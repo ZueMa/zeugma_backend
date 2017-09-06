633d7ccf3f598b13adb494aa7ce3ae2bafff8a81
Chanon Jenakom <chanonjenakom@gmail.com>
2017-09-06 13:41:06 +0700

    Create test suites - 'Retrieve Cart' API

--- src/buyers/tests.py
+++ src/buyers/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase, Client
-from .models import Buyer
+from .models import Buyer, Cart
 
 import json
 
@@ -19,6 +19,9 @@ class BuyersTestCase(TestCase):
             address='100 Universal City Plaza, Universal City, Los Angeles'
         )
         self.buyer.save()
+
+        self.cart = Cart(buyer=self.buyer)
+        self.cart.save()
 
     def test_buyer_should_register_with_the_right_credentials(self):
         response = self.client.post(
@@ -61,6 +64,35 @@ class BuyersTestCase(TestCase):
         second_response = self.client.put('/buyers/1/')
         third_response = self.client.patch('/buyers/1/')
         fourth_response = self.client.delete('/buyers/1/')
+
+        self.assertEqual(first_response.status_code, 405)
+        self.assertEqual(second_response.status_code, 405)
+        self.assertEqual(third_response.status_code, 405)
+        self.assertEqual(fourth_response.status_code, 405)
+
+    def text_buyer_should_retrieve_their_cart(self):
+        response = self.client.get('/buyers/1/cart/')
+
+        self.assertEqual(response.json()['cart_id'], 1)
+        self.assertEqual(response.json()['total_price'], 0)
+        self.assertIsNotNone(response.json()['items'])
+        self.assertEqual(response.status_code, 200)
+
+    def test_buyer_should_create_new_cart_if_unpurchased_cart_does_not_exist(self):
+        self.cart.is_purchased = True
+        self.cart.save(update_fields=['is_purchased'])
+        response = self.client.get('/buyers/1/cart/')
+
+        self.assertEqual(response.json()['cart_id'], 2)
+        self.assertEqual(response.json()['total_price'], 0)
+        self.assertIsNotNone(response.json()['items'])
+        self.assertEqual(response.status_code, 200)
+
+    def test_server_should_return_405_with_wrong_HTTP_methods_for_buyer_cart(self):
+        first_response = self.client.post('/buyers/1/cart/')
+        second_response = self.client.put('/buyers/1/cart/')
+        third_response = self.client.patch('/buyers/1/cart/')
+        fourth_response = self.client.delete('/buyers/1/cart/')
 
         self.assertEqual(first_response.status_code, 405)
         self.assertEqual(second_response.status_code, 405)
