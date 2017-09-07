30a082d8f26ddb1c3effbd94380e4dd39eba8b80
Chanon Jenakom <chanonjenakom@gmail.com>
2017-09-07 12:50:56 +0700

    Create test suites - 'Retrieve Seller's Products' API

--- src/sellers/tests.py
+++ src/sellers/tests.py
@@ -1,5 +1,6 @@
 from django.test import TransactionTestCase, Client
 from .models import Seller
+from src.products.models import Product
 
 import json
 
@@ -20,6 +21,42 @@ class SellersTestCase(TransactionTestCase):
             description='The Brotherhood was founded by Magneto and its members were his primary allies in his early battles with the X-Men during the 1960s. The original Brotherhood ultimately disbanded, with Quicksilver and Scarlet Witch going on to become members of the Avengers.'
         )
         self.seller.save()
+
+        self.product_1 = Product(
+            name='Cerebro',
+            category='Cosmetics',
+            price=1749.99,
+            num_stocks=3,
+            short_description='Read minds across the globe!',
+            full_description='Cerebro is a fictional device appearing in American comic books published by Marvel Comics. The device is used by the X-Men (in particular, their leader, Professor Charles Xavier) to detect humans, specifically mutants.',
+            image='http://localhost:8000/images/cerebro.jpg',
+            seller=self.seller
+        )
+        self.product_1.save()
+
+        self.product_2 = Product(
+            name='Invisibility Cloak',
+            category='Clothes',
+            price=799.99,
+            num_stocks=3,
+            short_description='Hide from anything, even death!',
+            full_description='An invisibility cloak is a magical garment which renders whomever or whatever it covers unseeable. These are common items that are massed produced in the wizarding world. The first known cloak was made by Death for Ignotus Peverell in the 13th century and it is one of a kind.',
+            image='http://localhost:8000/images/invisibility_cloak.jpg',
+            seller=self.seller
+        )
+        self.product_2.save()
+
+        self.product_3 = Product(
+            name='Mjolnir',
+            category='Sports',
+            price=2499.99,
+            num_stocks=3,
+            short_description='Weight-lifting like never before!',
+            full_description='In Norse mythology, Mjolnir is the hammer of Thor, a major Norse god associated with thunder. Mjolnir is depicted in Norse mythology as one of the most fearsome and powerful weapons in existence, capable of leveling mountains.',
+            image='http://localhost:8000/images/mjolnir.jpg',
+            seller=self.seller
+        )
+        self.product_3.save()
 
     def test_seller_should_register_with_the_right_credentials(self):
         response = self.client.post(
@@ -71,3 +108,28 @@ class SellersTestCase(TransactionTestCase):
         self.assertEqual(second_response.status_code, 405)
         self.assertEqual(third_response.status_code, 405)
         self.assertEqual(fourth_response.status_code, 405)
+
+    def test_seller_should_retrieve_their_prodcuts(self):
+        response = self.client.get('/sellers/1/products/')
+
+        self.assertIsNotNone(response.json()['products'])
+        self.assertEqual(len(response.json()['products']), 3)
+        self.assertEqual(response.json()['products'][0]['product_id'], 1)
+        self.assertEqual(response.json()['products'][0]['name'], 'Cerebro')
+        self.assertEqual(response.json()['products'][0]['category'], 'Cosmetics')
+        self.assertEqual(response.json()['products'][0]['price'], 1749.99)
+        self.assertEqual(response.json()['products'][0]['short_description'], 'Read minds across the globe!')
+        self.assertEqual(response.json()['products'][0]['image'], 'http://localhost:8000/images/cerebro.jpg')
+        self.assertEqual(response.json()['products'][1]['product_id'], 2)
+        self.assertEqual(response.json()['products'][1]['name'], 'Invisibility Cloak')
+        self.assertEqual(response.json()['products'][1]['category'], 'Clothes')
+        self.assertEqual(response.json()['products'][1]['price'], 799.99)
+        self.assertEqual(response.json()['products'][1]['short_description'], 'Hide from anything, even death!')
+        self.assertEqual(response.json()['products'][1]['image'], 'http://localhost:8000/images/invisibility_cloak.jpg')
+        self.assertEqual(response.json()['products'][2]['product_id'], 3)
+        self.assertEqual(response.json()['products'][2]['name'], 'Mjolnir')
+        self.assertEqual(response.json()['products'][2]['category'], 'Sports')
+        self.assertEqual(response.json()['products'][2]['price'], 2499.99)
+        self.assertEqual(response.json()['products'][2]['short_description'], 'Weight-lifting like never before!')
+        self.assertEqual(response.json()['products'][2]['image'], 'http://localhost:8000/images/mjolnir.jpg')
+        self.assertEqual(response.status_code, 200)
