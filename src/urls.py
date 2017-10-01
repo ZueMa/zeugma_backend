b5472ebaac423bbd91fe95adc6ce100efcfabb4a
Chanon Jenakom <chanonjenakom@gmail.com>
2017-10-01 15:52:43 +0700

    Remove site admin pages

--- src/urls.py
+++ src/urls.py
@@ -4,7 +4,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^authentication/', include('src.authentication.urls')),
     url(r'^buyers/', include('src.buyers.urls')),
     url(r'^products/', include('src.products.urls')),
