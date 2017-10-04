50be18e180637123fff56033ea1a92a869b17334
Chanon Jenakom <chanonjenakom@gmail.com>
2017-10-04 23:30:10 +0700

    Include 'out of stock' products

--- src/products/views.py
+++ src/products/views.py
@@ -7,7 +7,7 @@ def retrieve_all_products(request):
     if (request.method != 'GET'):
         return HttpResponse(status=405)
 
-    products_list = Product.objects.exclude(num_stocks=0).order_by('id')
+    products_list = Product.objects.order_by('id')
     products = []
 
     for product in products_list:
