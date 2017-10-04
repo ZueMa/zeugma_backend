300307e328a571c1c53d48340ee5a2af8b52cf4f
Chanon Jenakom <chanonjenakom@gmail.com>
2017-10-04 16:37:47 +0700

    Update 'Buyers' models

--- src/buyers/models.py
+++ src/buyers/models.py
@@ -7,9 +7,6 @@ class Buyer(models.Model):
     last_name = models.CharField(max_length=32)
     address = models.TextField()
 
-    def __str__(self):
-        return 'Buyer #{}'.format(self.id)
-
 class Cart(models.Model):
     buyer = models.ForeignKey('Buyer')
     is_purchased = models.BooleanField(default=False)
@@ -19,22 +16,13 @@ class Cart(models.Model):
         through_fields=('cart', 'product')
     )
 
-    def __str__(self):
-        return 'Cart #{}'.format(self.id)
-
 class ProductCart(models.Model):
     product = models.ForeignKey('products.Product')
     cart = models.ForeignKey('Cart')
     num_items = models.IntegerField(default=1)
-
-    def __str__(self):
-        return 'ProductCart #{}'.format(self.id)
 
 class Purchase(models.Model):
     buyer = models.ForeignKey('Buyer')
     cart = models.ForeignKey('Cart')
     is_shipped = models.BooleanField(default=False)
     timestamp = models.DateField(auto_now_add=True)
-
-    def __str__(self):
-        return 'Purchase #{}'.format(self.id)
