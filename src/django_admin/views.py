a8bcdc4705811110355fd6147167343afc92b2e2
Chanon Jenakom <chanonjenakom@gmail.com>
2017-10-31 11:26:18 +0700

    Finish admin sign out

--- src/django_admin/views.py
+++ src/django_admin/views.py
@@ -5,12 +5,14 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def authenticate(request):
-    if (request.method != 'POST'):
-        return HttpResponse(status=405)
-
-    request_body = json.loads(request.body.decode('utf-8'))
-    
-    if (request_body['username'] == 'Admin' and request_body['password'] == 'ZueMaAdmin'):
+    if (request.method == 'POST'):
+        request_body = json.loads(request.body.decode('utf-8'))
+        
+        if (request_body['username'] == 'Admin' and request_body['password'] == 'ZueMaAdmin'):
+            return HttpResponse(status=204)
+        else:
+            return HttpResponse(status=404)
+    elif (request.method == 'DELETE'):
         return HttpResponse(status=204)
     else:
-        return HttpResponse(status=404)
+        return HttpResponse(status=405)
