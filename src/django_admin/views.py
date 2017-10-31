2a266e30735e56979a7cdfe97b2ef0f109f83105
Chanon Jenakom <chanonjenakom@gmail.com>
2017-10-31 11:23:44 +0700

    Finish admin sign in

--- src/django_admin/views.py
+++ src/django_admin/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def authenticate(request):
+    if (request.method != 'POST'):
+        return HttpResponse(status=405)
+
+    request_body = json.loads(request.body.decode('utf-8'))
+    
+    if (request_body['username'] == 'Admin' and request_body['password'] == 'ZueMaAdmin'):
+        return HttpResponse(status=204)
+    else:
+        return HttpResponse(status=404)
